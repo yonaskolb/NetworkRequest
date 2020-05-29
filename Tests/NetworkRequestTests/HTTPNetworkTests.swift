eecd103f8bc345881a44714dabcb3acd2ffae487
Yonas Kolb <yonaskolb@gmail.com>
2020-05-29 13:09:04 +1000

    allow more time for jsonplaceholder.typicode.com

--- Tests/NetworkRequestTests/HTTPNetworkTests.swift
+++ Tests/NetworkRequestTests/HTTPNetworkTests.swift
@@ -19,7 +19,7 @@ class HTTPNetworkRequestTests: XCTestCase {
             }
             expectation.fulfill()
         }
-        wait(for: [expectation], timeout: 15)
+        wait(for: [expectation], timeout: 60)
     }
 
     func testNetworkRequestEncoding() throws {
@@ -31,7 +31,7 @@ class HTTPNetworkRequestTests: XCTestCase {
         networkService.makeRequest(request) { result in
             expectation.fulfill()
         }
-        wait(for: [expectation], timeout: 15)
+        wait(for: [expectation], timeout: 60)
     }
 }
 
